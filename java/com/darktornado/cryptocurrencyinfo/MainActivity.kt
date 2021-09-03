cc6a1fa4d2fb4a7616b44f75dae7ca36030bd764
Dark Tornado <dt3141592@naver.com>
2021-09-03 23:09:38 +0900

    init

---
+++ java/com/darktornado/cryptocurrencyinfo/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.darktornado.cryptocurrencyinfo
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
